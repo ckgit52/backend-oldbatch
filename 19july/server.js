f32220ccf9871858d31a01adea0a1607d0f0cbbd
chandan <chandankumar.btele20@pec.edu.in>
2024-07-23 21:53:00 +0530

    done crud on temporary array of data

--- 19july/server.js
+++ 19july/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
+app.use(express.json());
 
-let data=["satya"]
+let data=["satya","chandan","kumar"]
 
 app.get('/', (req, res) => {
   res.send(data)
@@ -27,27 +28,22 @@ app.delete('/home/:index', (req, res) => {
     console.log(index)
 
     data.splice(index,1);
+    
 
     res.send(data)
   })
 
   app.put('/home/:index', (req, res) => {
-    let index = parseInt(req.params.index, 10);
+    let index = parseInt(req.params.index,10);
     console.log(index);
 
-    // Assuming data is defined and is an array
-    if (!Array.isArray(data)) {
-        return res.status(500).send("Internal server error");
-    }
+   
 
     if (index >= data.length || index < 0) {
         return res.status(404).send("Out of bound index");
     }
 
-    if (!req.body.name) {
-        return res.status(400).send("Name is required");
-    }
-
+  
     data[index] = req.body.name;
 
     res.send(data);
